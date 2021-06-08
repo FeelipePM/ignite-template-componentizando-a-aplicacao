5264f281353dcd11a5d0da14cddedee3c598f04a
Felipe <Feelipe@email.com>
2021-06-07 23:22:47 -0300

    Resolved Challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import React, { FC } from 'react';
+
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+export const Content: FC<ContentProps> = ({ movies }) => {
+  return (
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React, { FC }from 'react';
+
+import '../styles/header.scss';
+
+interface HeaderProps {
+  title: string;
+}
+
+export const Header: FC<HeaderProps> = ({ title }) => {
+  return (
+    <header>
+      <span className="category">Categoria:<span>{title}</span></span>
+    </header>
+  )
+}
